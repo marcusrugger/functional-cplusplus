6f854c08374e2aa7a40c429a42401280a8f51a91
Marcus Rugger <marcusrugger@yahoo.com>
2014-01-18 16:39:22 -0500

    Added append vector unit test

--- src/test.vector.cpp
+++ src/test.vector.cpp
@@ -49,10 +49,58 @@ static void test_vector_back_iterator(void)
 }
 
 
+static void test_append_vector(void)
+{
+  using string = mtr::string;
+  using vector = mtr::vector<string>;
+
+  const char *strs[] = { "string 1",
+                         "string 2",
+                         "string 3",
+                         "string 4" };
+
+  vector a = vector().append(string(strs[0]))
+                     .append(string(strs[1]));
+
+  vector b = vector().append(string(strs[2]))
+                     .append(string(strs[3]));
+
+  SHOULD_BE_EQ((int) a.length(), 2, "vector a should have two items");
+  SHOULD_BE_EQ((int) b.length(), 2, "vector b should have two items");
+
+  vector c = a.append(b);
+
+  SHOULD_BE_EQ((int) a.length(), 2, "vector a should have two items");
+  SHOULD_BE_EQ((int) b.length(), 2, "vector b should have two items");
+  SHOULD_BE_EQ((int) c.length(), 4, "vector c should have four items");
+
+  auto cmp_fn = [&](int a, string s)
+  {
+    SHOULD_BE_EQ(s, string(strs[a]), "Strings should be equal");
+    return a+1;
+  };
+  c.foreach(0)(cmp_fn);
+
+#if 0
+  auto it = c.iterator();
+  auto s1 = it();
+  auto s2 = it.next()();
+  auto s3 = it.next().next()();
+  auto s4 = it.next().next().next()();
+
+  SHOULD_BE_EQ(s1, string(str1), "");
+  SHOULD_BE_EQ(s2, string(str2), "");
+  SHOULD_BE_EQ(s3, string(str3), "");
+  SHOULD_BE_EQ(s4, string(str4), "");
+#endif
+}
+
+
 void test_vector(void)
 {
   std::cout << "BEGIN: ***** " << __FILE__ << " *****" << std::endl;
   test_vector_append_with_strings();
   test_vector_back_iterator();
+  test_append_vector();
   std::cout << "END:   ***** " << __FILE__ << " *****" << std::endl;
 }
