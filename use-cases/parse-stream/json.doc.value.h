13892ebba88921a4abf24cfd46d84feeb8083289
Marcus Rugger <marcusrugger@yahoo.com>
2014-01-27 08:19:16 -0500

    Update

--- use-cases/parse-stream/json.doc.value.h
+++ use-cases/parse-stream/json.doc.value.h
@@ -6,6 +6,7 @@ class json_doc_value
 public:
 
   using string = mtr::string;
+  using input_iterator = mtr::index_forward_iterator<mtr::string,char>;
 
   virtual string to_s(void) const = 0;
   virtual json_doc_value *clone(void) const = 0;
